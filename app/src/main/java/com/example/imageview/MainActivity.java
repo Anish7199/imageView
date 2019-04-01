0104a5f5f579e8b65625d073df9141cb6d28495c
Anishsthapit7199 <anishsthapit@outlook.com>
2019-04-01 11:43:25 +0545

    imaged obtained

--- app/src/main/java/com/example/imageview/MainActivity.java
+++ app/src/main/java/com/example/imageview/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.imageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private Button rdoOne, rdoTwo, rdoThree ;
+    private ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        rdoOne = findViewById(R.id.rdoOne);
+        rdoTwo = findViewById(R.id.rdoTwo);
+        rdoThree = findViewById(R.id.rdoThree);
+        imgView = findViewById(R.id.imgView);
+
+        rdoOne.setOnClickListener(this);
+        rdoTwo.setOnClickListener(this);
+        rdoThree.setOnClickListener(this);
     }
-}
+            @Override
+            public void onClick(View v) {
+                switch (v.getId())
+                {
+                    case R.id.rdoOne:
+                        imgView.setImageResource(R.drawable.images);
+                        break;
+
+
+
+
+            }
+        }
+    }
+
